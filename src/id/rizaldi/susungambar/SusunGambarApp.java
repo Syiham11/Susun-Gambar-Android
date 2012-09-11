7286adbe372fc722406af127cc9e6d122e115205
Rizaldi Martaputra <rizaldi.m@gmail.com>
2012-09-11 08:45:14 +0700

    initial commit

---
+++ src/id/rizaldi/susungambar/SusunGambarApp.java
@@ -0,0 +1,15 @@
+package id.rizaldi.susungambar;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SusunGambarApp extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
